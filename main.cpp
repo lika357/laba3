5d9fc1d6a59693ae90782fa4fd2665d3b8927f71
Анжелика <anhamenkova@gmail.com>
2026-05-18 00:34:21 +0300

    класс кнопки как в прошлой лабе и изменение текста в кнопке и проверка нажатия мышкой

--- main.cpp
+++ main.cpp
@@ -5,6 +5,46 @@
 
 #include "queue.hpp"
 #include "stack.hpp"
+class Button
+{
+   private:
+    sf::RectangleShape shape;
+    sf::Text text;
+
+   public:
+    Button(float x, float y, float w, float h, const std::string& str, sf::Font& font) : text(font)
+    {
+        shape.setPosition({x, y});
+        shape.setSize({w, h});
+        shape.setFillColor(sf::Color::White);
+        shape.setOutlineThickness(2);
+        shape.setOutlineColor(sf::Color(101, 67, 33));
+        text.setString(str);
+        text.setCharacterSize(22);
+        text.setFillColor(sf::Color::Black);
+        sf::FloatRect b = text.getLocalBounds();
+        text.setPosition({x + w / 2 - b.size.x / 2, y + h / 2 - b.size.y / 2 - 5});
+    }
+
+    void setText(const std::string& str)
+    {
+        text.setString(str);
+        sf::FloatRect b = text.getLocalBounds();
+        text.setPosition({shape.getPosition().x + shape.getSize().x / 2 - b.size.x / 2,
+                          shape.getPosition().y + shape.getSize().y / 2 - b.size.y / 2 - 5});
+    }
+
+    bool clicked(sf::Vector2f m)
+    {
+        return shape.getGlobalBounds().contains(m);
+    }
+
+    void draw(sf::RenderWindow& w)
+    {
+        w.draw(shape);
+        w.draw(text);
+    }
+};
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({900, 650}), "LABA 3");
